d1b44cb073ecc610b10fc43735e8422bd0bb1491
Emesson <emessonbarbosa600@gmail.com>
2024-04-06 22:47:23 -0300

    SW upgraded

--- sw.js
+++ sw.js
@@ -1,55 +1,62 @@
-const cacheName = "sw-page-v1";
+const cacheName = "sw-page";
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(cacheName)
-      .then(cache => cache.addAll([
-        './index.html',
-        './style.css',
-        './assets/webapp-logo.avif',
-        './assets/icons/android/android-launchericon-48-48.png',
-        './assets/icons/android/android-launchericon-72-72.png',
-        './assets/icons/android/android-launchericon-96-96.png',
-        './assets/icons/android/android-launchericon-144-144.png',
-        './assets/icons/android/android-launchericon-512-512.png'
-      ]))
+      .then(cache => {
+        return cache.addAll([
+          './index.html',
+          './style.css',
+          './assets/webapp-logo.avif',
+          './assets/icons/android/android-launchericon-48-48.png',
+          './assets/icons/android/android-launchericon-72-72.png',
+          './assets/icons/android/android-launchericon-96-96.png',
+          './assets/icons/android/android-launchericon-144-144.png',
+          './assets/icons/android/android-launchericon-192-192.png',
+          './assets/icons/android/android-launchericon-512-512.png',
+          './assets/icons/ios/16.png',
+          './assets/icons/ios/20.png',
+          './assets/icons/ios/29.png',
+          './assets/icons/ios/32.png',
+          './assets/icons/ios/40.png',
+          './assets/icons/ios/50.png',
+          './assets/icons/ios/57.png',
+          './assets/icons/ios/58.png',
+          './assets/icons/ios/60.png',
+          './assets/icons/ios/64.png',
+          './assets/icons/ios/72.png',
+          './assets/icons/ios/76.png',
+          './assets/icons/ios/80.png',
+          './assets/icons/ios/87.png',
+          './assets/icons/ios/100.png',
+          './assets/icons/ios/114.png',
+          './assets/icons/ios/120.png',
+          './assets/icons/ios/128.png',
+          './assets/icons/ios/167.png',
+          './assets/icons/ios/180.png',
+          './assets/icons/ios/192.png',
+          './assets/icons/ios/256.png',
+          './assets/icons/ios/512.png',
+          './assets/icons/ios/1024.png'
+        ]);
+      })
   );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(cache => cache !== cacheName).map(cache => caches.delete(cache))
-      );
-    })
-  );
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request).then(fetchResponse => {
-        return caches.open(cacheName).then(cache => {
-          cache.put(event.request, fetchResponse.clone());
-          return fetchResponse;
-        });
-      });
-    }).catch(error => {
-      console.error('Error fetching:', error);
-      return new Response('No internet connection', {
-        status: 503,
-        statusText: 'Service Unavailable',
-        headers: new Headers({
-          'Content-Type': 'text/plain'
-        })
-      });
-    })
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+      .catch(error => {
+        console.error('Error in fetching:', error);
+      })
   );
-});
-
-self.addEventListener('message', event => {
-  if (event.data.action === 'skipWaiting') {
-    self.skipWaiting();
-  }
 });
